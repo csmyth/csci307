b310c62f4f64e6c31ef9e849d6952f3bfd96862b
Cecilia Smyth <csmyth@mymail.mines.edu>
2012-05-18 11:46:07 -0600

    App generated

---
+++ AGWMobile/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AGWMobile::Application.config.secret_token = '8ee6411a15b00ac270428cc6ce6eb01efe24860f930388d4c815d7e98cb3223bee803d2686ed4fbc793314b22d7207864571e20fe49ac6248f1b1498825e471f'
