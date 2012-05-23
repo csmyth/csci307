12802cfa7a2dc0e6c646507980eb5c58c69cab07
Jimmie Aaron Butler <jimmiebtlr@gmail.com>
2012-05-23 08:16:28 -0600

    Temporarily disabled cancan require check all.

--- AGWMobile/app/controllers/users_controller.rb
+++ AGWMobile/app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!, :only => [:new, :create, :cancel]
-  skip_before_filter :require_no_authentication
+  load_and_authorize_resource
   # GET /users
   # GET /users.json
   def index
