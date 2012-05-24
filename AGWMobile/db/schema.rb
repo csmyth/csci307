adb559968b75968be3f29cc6d90614b7de60dc5e
Cecilia Smyth <csmyth@mymail.mines.edu>
2012-05-23 19:41:47 -0600

    Header, footer, and application-wide styling added via Bootstrap, not complete

--- AGWMobile/db/schema.rb
+++ AGWMobile/db/schema.rb
@@ -11,49 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120523170137) do
-
-  create_table "assignments", :force => true do |t|
-    t.integer  "user_id",                           :null => false
-    t.integer  "project_id",                        :null => false
-    t.integer  "observation_id",                    :null => false
-    t.boolean  "archived",       :default => false
-    t.date     "archive_date"
-    t.datetime "created_at",                        :null => false
-    t.datetime "updated_at",                        :null => false
-  end
-
-  create_table "clients", :force => true do |t|
-    t.string   "company_name",                    :null => false
-    t.string   "address1",                        :null => false
-    t.string   "address2"
-    t.string   "city",                            :null => false
-    t.string   "state",                           :null => false
-    t.string   "zipcode",                         :null => false
-    t.string   "phone"
-    t.string   "fax"
-    t.boolean  "PO_required",  :default => true
-    t.boolean  "archived",     :default => false
-    t.date     "archive_date"
-    t.datetime "created_at",                      :null => false
-    t.datetime "updated_at",                      :null => false
-  end
-
-  create_table "contacts", :force => true do |t|
-    t.integer  "company_id",                        :null => false
-    t.boolean  "is_primary",     :default => false
-    t.string   "first_name",                        :null => false
-    t.string   "middle_name"
-    t.string   "last_name",                         :null => false
-    t.string   "title"
-    t.string   "email",                             :null => false
-    t.string   "cell_phone"
-    t.string   "business_phone"
-    t.boolean  "archived",       :default => false
-    t.date     "archive_date"
-    t.datetime "created_at",                        :null => false
-    t.datetime "updated_at",                        :null => false
-  end
+ActiveRecord::Schema.define(:version => 20120518183321) do
 
   create_table "observations", :force => true do |t|
     t.decimal  "def_fee",      :precision => 8, :scale => 2,                    :null => false
@@ -64,68 +22,31 @@ ActiveRecord::Schema.define(:version => 20120523170137) do
     t.datetime "updated_at",                                                    :null => false
   end
 
-  create_table "projects", :force => true do |t|
-    t.integer  "client_id",                             :null => false
-    t.integer  "project_number",                        :null => false
-    t.integer  "primary_contact_id",                    :null => false
-    t.integer  "billing_contact_id"
-    t.integer  "letter_contact_id"
-    t.boolean  "PO_per_project"
-    t.string   "address1"
-    t.string   "address2"
-    t.string   "city"
-    t.string   "state"
-    t.string   "zipcode"
-    t.string   "subdivision"
-    t.integer  "lot"
-    t.integer  "block"
-    t.integer  "filing"
-    t.integer  "building"
-    t.integer  "unit"
-    t.boolean  "archived",           :default => false
-    t.date     "archive_date"
-    t.text     "description"
-    t.datetime "created_at",                            :null => false
-    t.datetime "updated_at",                            :null => false
-  end
-
-  create_table "red_flags", :force => true do |t|
-    t.integer  "client_id"
-    t.integer  "project_id"
-    t.text     "description",                     :null => false
-    t.boolean  "archived",     :default => false
-    t.date     "archive_date"
-    t.datetime "created_at",                      :null => false
-    t.datetime "updated_at",                      :null => false
-  end
-
-  create_table "roles", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "roles_users", :id => false, :force => true do |t|
-    t.integer "role_id"
-    t.integer "user_id"
-  end
-
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "first_name",                             :null => false
+    t.string   "middle_name"
+    t.string   "last_name",                              :null => false
+    t.string   "title"
+    t.string   "certification"
+    t.string   "phone"
+    t.boolean  "archived",            :default => false
+    t.date     "archive_date"
+    t.string   "login",                                  :null => false
+    t.string   "email",                                  :null => false
+    t.string   "crypted_password",                       :null => false
+    t.string   "password_salt",                          :null => false
+    t.string   "persistence_token",                      :null => false
+    t.string   "single_access_token",                    :null => false
+    t.string   "perishable_token",                       :null => false
+    t.integer  "login_count",         :default => 0,     :null => false
+    t.integer  "failed_login_count",  :default => 0,     :null => false
+    t.datetime "last_request_at"
+    t.datetime "current_login_at"
+    t.datetime "last_login_at"
+    t.string   "current_login_ip"
+    t.string   "last_login_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
   end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
