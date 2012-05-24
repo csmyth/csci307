64860b301382a0712ed258994d39a7e5ddef580d
Cecilia Smyth <csmyth@mymail.mines.edu>
2012-05-23 21:18:01 -0600

    Changed root route. Temporarily set to assignments#index

--- AGWMobile/config/routes.rb
+++ AGWMobile/config/routes.rb
@@ -64,6 +64,7 @@ AGWMobile::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => 'assignments#index'
 
   # See how all your routes lay out with "rake routes"
 
